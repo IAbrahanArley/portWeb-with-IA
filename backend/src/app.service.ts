a3b154c10150ea12a1e54de024220dd06129c17e
IAbrahanarley <abrahanarley96@gmail.com>
2025-03-18 21:25:30 -0300

    Feat: exportando modelos do projeto

--- backend/src/app.service.ts
+++ backend/src/app.service.ts
@@ -1,8 +1,8 @@
 import { Injectable } from '@nestjs/common';
-
+import { Id } from '../../core/src/index';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Hello World!';
+    return 'Hello World!' + Id.gerar();
   }
 }
