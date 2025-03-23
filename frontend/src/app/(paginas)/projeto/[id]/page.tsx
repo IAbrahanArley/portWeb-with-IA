d5d7de53449ba3a99811e93bc5ec7f4e064670c7
IAbrahanarley <abrahanarley96@gmail.com>
2025-03-23 12:56:52 -0300

    Feat: exibindo detalhes do projeto

--- frontend/src/app/(paginas)/projeto/[id]/page.tsx
+++ frontend/src/app/(paginas)/projeto/[id]/page.tsx
@@ -1,15 +1,23 @@
+import { getProjeto } from "@/components/functions/projetos"
 import Cabecalho from "@/components/shared/Cabecalho"
+import CarrosselImagens from "@/components/shared/CarrosselImagens"
+import Container from "@/components/shared/Container"
 import React from "react"
 
 const PaginaProjeto = async (props: { params: Promise<{ id: string }> }) => {
-	console.log(props)
 	const { id } = await props.params
-	return (
-		<div>
+	const projeto = await getProjeto(id)
+	return projeto ? (
+		<div className="bg-black text-white">
 			<Cabecalho />
-			<h1>Projeto {id}</h1>
+			<Container className="py-7 flex flex-col gap-10">
+				<h1 className="text-3xl font-bold">{projeto.nome}</h1>
+				<div className="flex flex-col items-center">
+					<CarrosselImagens imagens={projeto.imagens.slice(1)} />
+				</div>
+			</Container>
 		</div>
-	)
+	) : null
 }
 
 export default PaginaProjeto
---
+++ frontend/src/components/shared/CarrosselImagens.tsx
@@ -0,0 +1,30 @@
+import Image from "next/image"
+import {
+	Carousel,
+	CarouselContent,
+	CarouselItem,
+	CarouselNext,
+	CarouselPrevious,
+} from "../ui/carousel"
+
+export interface CarrosselImagensProps {
+	imagens: string[]
+}
+
+const CarrosselImagens = (props: CarrosselImagensProps) => {
+	return (
+		<Carousel opts={{ loop: true }} className="md:w-11/12 xl:w-full w-7/10">
+			<CarouselContent>
+				{props.imagens.map((imagem, index) => (
+					<CarouselItem key={index} className="relative h-96 w-full">
+						<Image src={imagem} alt="Imagem" fill className="object-cover" />
+					</CarouselItem>
+				))}
+			</CarouselContent>
+			<CarouselNext />
+			<CarouselPrevious />
+		</Carousel>
+	)
+}
+
+export default CarrosselImagens
